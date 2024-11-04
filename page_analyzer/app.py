237f453a29f69ed79f754d881a8bde515e54295a
Fedor.Shurgin <shurginfedor@gmail.com>
2024-11-04 18:32:59 +0300

    The second step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from jinja2 import Environment, FileSystemLoader
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+file_loader = FileSystemLoader('page_analyzer') #загрузчик
+env = Environment(loader=file_loader) #окружение
+
+tm_index = env.get_template('index.html')
+tm_index  .render()

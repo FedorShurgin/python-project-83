37a82be77e30fce242229d3976da862d5afe3cb3
Fedor.Shurgin <shurginfedor@gmail.com>
2024-11-04 18:54:28 +0300

    fix app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+'''
 file_loader = FileSystemLoader('page_analyzer') #загрузчик
 env = Environment(loader=file_loader) #окружение
 
 tm_index = env.get_template('index.html')
 tm_index.render()
+'''

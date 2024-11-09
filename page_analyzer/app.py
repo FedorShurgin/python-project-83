2e705e65b21dfa5c1f0665fb7669381b0b7cd424
Fedor.Shurgin <shurginfedor@gmail.com>
2024-11-09 19:07:26 +0300

    add app.py and urls.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+
+
+list_urls = [{'id': 'id', 'name': 'name_url', 'last_check': 'data', 'response_code': 'code'},
+            {'id': 'id', 'name': 'name_url', 'last_check': 'data', 'response_code': 'code'},
+            {'id': 'id', 'name': 'name_url', 'last_check': 'data', 'response_code': 'code'}
+        ]
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/urls")
+def urls_get():
+    return render_template('urls.html', list_urls=list_urls)
 
 
 if __name__ == "__main__":
